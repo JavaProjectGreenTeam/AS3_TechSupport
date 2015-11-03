86db479f11cf63bc91ccc7d977c539ff5dc03a55
Nick Lambell <lambell.nicholas95@gmail.com>
2015-11-03 11:27:54 +1000

    Fixed page formatting, began incidents page code

--- AS3_TechSupport/AddRegistration.aspx
+++ AS3_TechSupport/AddRegistration.aspx
@@ -21,7 +21,7 @@
             <td class="auto-style2">
                 <asp:DropDownList ID="ddlCustomer" runat="server" DataSourceID="AddRegistrationCustomer" DataTextField="Name" DataValueField="Name">
                 </asp:DropDownList>
-                <asp:SqlDataSource ID="AddRegistrationCustomer" runat="server" ConnectionString="<%$ ConnectionStrings:TechSupportConnectionString %>" SelectCommand="SELECT [Name] FROM [Customers]"></asp:SqlDataSource>
+                <asp:SqlDataSource ID="AddRegistrationCustomer" runat="server" ConnectionString="<%$ ConnectionStrings:TechSupportDB %>" SelectCommand="SELECT [Name] FROM [Customers]"></asp:SqlDataSource>
             </td>
             <td class="auto-style3">&nbsp;</td>
         </tr>
@@ -32,7 +32,7 @@
             <td class="auto-style2">
                 <asp:DropDownList ID="ddlProduct" runat="server" DataSourceID="AddRegistrationProduct" DataTextField="Name" DataValueField="Name">
                 </asp:DropDownList>
-                <asp:SqlDataSource ID="AddRegistrationProduct" runat="server" ConnectionString="<%$ ConnectionStrings:TechSupportConnectionString %>" SelectCommand="SELECT [Name] FROM [Products]"></asp:SqlDataSource>
+                <asp:SqlDataSource ID="AddRegistrationProduct" runat="server" ConnectionString="<%$ ConnectionStrings:TechSupportDB %>" SelectCommand="SELECT [Name] FROM [Products]"></asp:SqlDataSource>
             </td>
             <td class="auto-style3">&nbsp;</td>
         </tr>
@@ -51,10 +51,10 @@
             </td>
             <td class="auto-style2">
                 <asp:Button ID="btnCancel" runat="server" CssClass="Login" Font-Size="Small" Text="Cancel" />
-            </td>
-            <td class="auto-style3">
                 <asp:Button ID="btnExit" runat="server" CssClass="Login" Font-Size="Small" Text="Exit" Width="94px" />
             </td>
+            <td class="auto-style3">
+                &nbsp;</td>
         </tr>
     </table>
 </asp:Content>
--- AS3_TechSupport/Tech/CustomerIncidents.aspx.cs
+++ AS3_TechSupport/Tech/CustomerIncidents.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,24 +10,52 @@ namespace AS3_TechSupport
 {
     public partial class CustomerIncidents : System.Web.UI.Page
     {
+        protected DataView customerInfoOut =null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            //TechSupport.SelectCommand = "SELECT ProductCode, DateOpened, DateClosed, Title FROM Incidents WHERE CustomerID = " + lblCustomerID1.Text;
+            //sqlIncidents.SelectCommand = "SELECT ProductCode, DateOpened, DateClosed, Title FROM Incidents WHERE CustomerID = " + lblCustomerID1.Text;
             //SELECT [ProductCode], [DateOpened], [DateClosed], [Title] FROM [Incidents]
         }
 
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            int numRows = GridView1.Rows.Count;
         }
 
         protected void btnGetCustomer_Click(object sender, EventArgs e) {
-            if (txtCustomerID1.Text != "") {
-                TechSupport.SelectCommand = "SELECT ProductCode, DateOpened, DateClosed, Title FROM Incidents WHERE CustomerID = " + txtCustomerID1.Text;
-                TechSupport.Select(new DataSourceSelectArguments());
-                GridView1.Visible = true;
-            } else {
-                GridView1.Visible = false;
+                RunQuerys(txtCustomerID1.Text);
+        }
+
+        protected void btnTest_Click(object sender, EventArgs e) {
+            int numRows = GridView1.Rows.Count;
+            txtTest.Text = numRows.ToString();
+        }
+
+        protected void RunQuerys(string customerID) {
+            //Clear data source perameters
+            sqlCustomerInfo.SelectParameters.Clear();
+            sqlIncidents.SelectParameters.Clear();
+
+            //Add customerID as a perameter
+            sqlCustomerInfo.SelectParameters.Add("CustomerID", customerID);
+            sqlIncidents.SelectParameters.Add("CustomerID", customerID);
+
+            //Run select statements
+            DataView customerInfoOut = (DataView)sqlCustomerInfo.Select(new DataSourceSelectArguments());
+            sqlIncidents.Select(new DataSourceSelectArguments());
+        }
+
+        protected void sqlCustomerInfo_Selected(object sender, SqlDataSourceStatusEventArgs e) {
+            if (customerInfoOut != null) {
+                txtCustomerID2.Text = customerInfoOut["CustomerID"].ToString();
+                txtName.Text = customerInfoOut["CustomerID"].ToString();
+                txtAddress.Text = customerInfoOut["CustomerID"].ToString();
+                txtCity.Text = customerInfoOut["CustomerID"].ToString();
+                txtState.Text = customerInfoOut["CustomerID"].ToString();
+                txtZip.Text = customerInfoOut["CustomerID"].ToString();
+                txtPhone.Text = customerInfoOut["CustomerID"].ToString();
+                txtEmail.Text = customerInfoOut["CustomerID"].ToString();
             }
         }
     }
