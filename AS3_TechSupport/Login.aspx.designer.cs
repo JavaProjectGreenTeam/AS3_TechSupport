22ea60cc769084ac406ee60bf2558044dd7ec20d
Nick Lambell <lambell.nicholas95@gmail.com>
2015-11-12 11:07:49 +1000

    Edited master page to have login related elements

--- AS3_TechSupport/Login.aspx.designer.cs
+++ AS3_TechSupport/Login.aspx.designer.cs
@@ -29,5 +29,32 @@ namespace AS3_TechSupport {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Login Login2;
+        
+        /// <summary>
+        /// LoginStatus1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginStatus LoginStatus1;
+        
+        /// <summary>
+        /// LoginName1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginName LoginName1;
+        
+        /// <summary>
+        /// LoginView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView1;
     }
 }
--- AS3_TechSupport/MasterPages/Main.Master.designer.cs
+++ AS3_TechSupport/MasterPages/Main.Master.designer.cs
@@ -31,6 +31,15 @@ namespace AS3_TechSupport.MasterPages {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// logName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginName logName;
+        
+        /// <summary>
         /// mnuNavBar control.
         /// </summary>
         /// <remarks>
@@ -38,6 +47,15 @@ namespace AS3_TechSupport.MasterPages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Menu mnuNavBar;
+        
+        /// <summary>
+        /// logStatus control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginStatus logStatus;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
